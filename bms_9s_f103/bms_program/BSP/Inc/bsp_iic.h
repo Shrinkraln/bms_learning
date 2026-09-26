915ed56948deb1fc41a51968f684f0492bb2a73a
Shrinkraln <2133275265@qq.com>
2026-09-26 06:11:47 +0000

    0925功能分析和分层设计

---
+++ bms_9s_f103/bms_program/BSP/Inc/bsp_iic.h
@@ -0,0 +1,10 @@
+#ifndef BSP_IIC_H
+#define BSP_IIC_H
+#include "main.h"
+#include "i2c.h"
+
+void vSample()
+void vCalcGetGainOffset(uint16_t *gain);
+
+
+#endif
---
+++ bms_9s_f103/bms_program/BSP/Src/bsp_iic.c
@@ -0,0 +1,6 @@
+#include "bsp_iic.h"
+
+//由于多个任务涉及iic，需要分别命名
+
+//计算
+void vCalcGetGainOffset(uint16_t *gain){}
